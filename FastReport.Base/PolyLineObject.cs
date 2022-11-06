defa336886e888d077f1f8da6f3a716dc93bc99c
Itan Kurteši <itan96@live.com>
2022-11-06 12:56:59 +0100

    Issue S1123

--- FastReport.Base/PolyLineObject.cs
+++ FastReport.Base/PolyLineObject.cs
@@ -66,7 +66,7 @@ namespace FastReport
         /// deprecated
         /// </summary>
         [Browsable(false)]
-        [Obsolete]
+        [Obsolete("Replaced by the automatic starter")]
         public PointF[] PointsArray
         {
             get
