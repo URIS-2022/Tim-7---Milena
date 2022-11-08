62b3aec0d83031e08839c9e79d1a48a89efd6b3c
Irena Ijacic <irenaijacic17@gmail.com>
2022-11-08 19:59:10 +0100

    Issue S1118

--- Demos/OpenSource/FastReport.OpenSource.Web.MVC/Program.cs
+++ Demos/OpenSource/FastReport.OpenSource.Web.MVC/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace FastReport.OpenSource.Web.MVC
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
