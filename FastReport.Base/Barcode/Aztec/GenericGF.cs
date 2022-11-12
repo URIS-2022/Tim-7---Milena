2c6b5c822bc30b2562b6d0bd8e4f83a0659a6d96
Irena Ijacic <irenaijacic17@gmail.com>
2022-11-12 12:06:58 +0100

    Issue S2223

--- FastReport.Base/Barcode/Aztec/GenericGF.cs
+++ FastReport.Base/Barcode/Aztec/GenericGF.cs
@@ -33,7 +33,7 @@ namespace FastReport.Barcode.Aztec
       public static GenericGF AZTEC_DATA_6 = new GenericGF(0x43, 64, 1); // x^6 + x + 1
       public static GenericGF AZTEC_PARAM = new GenericGF(0x13, 16, 1); // x^4 + x + 1
       public static GenericGF QR_CODE_FIELD_256 = new GenericGF(0x011D, 256, 0); // x^8 + x^4 + x^3 + x^2 + 1
-      public static GenericGF DATA_MATRIX_FIELD_256 = new GenericGF(0x012D, 256, 1); // x^8 + x^5 + x^3 + x^2 + 1
+      public static readonly GenericGF DATA_MATRIX_FIELD_256 = new GenericGF(0x012D, 256, 1); // x^8 + x^5 + x^3 + x^2 + 1
       public static GenericGF AZTEC_DATA_8 = DATA_MATRIX_FIELD_256;
       public static GenericGF MAXICODE_FIELD_64 = AZTEC_DATA_6;
 
