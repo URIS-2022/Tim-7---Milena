efab7d2a15070c14d1136d6a2e2801ee7d987076
Itan Kurteši <itan96@live.com>
2022-11-15 13:55:26 +0100

    Issue S1135 No.2

--- FastReport.Base/Utils/TextRenderer.cs
+++ FastReport.Base/Utils/TextRenderer.cs
@@ -2632,7 +2632,7 @@ namespace FastReport.Utils
         {
             // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
+            
             // GC.SuppressFinalize(this);
         }
         #endregion
